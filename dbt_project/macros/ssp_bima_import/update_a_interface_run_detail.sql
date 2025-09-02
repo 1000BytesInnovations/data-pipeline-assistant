59d393b6c9a75d7771f6e0f52f1c23bc6fafc6e5
vivek-1000bytes <vivek.kuppa@1000bytes.tech>
2025-09-02 21:18:02 +0530

    refactor: Remove SSP Bima import pipeline
    This commit removes all files related to the SSP Bima import pipeline, including dbt macros, models, and job definitions.

--- dbt_project/macros/ssp_bima_import/update_a_interface_run_detail.sql
+++
@@ -1,5 +0,0 @@
-{% macro update_a_interface_run_detail() %}
-UPDATE CBI_METADATA.A_INTERFACE_RUN
-SET LAST_RUN_DATE = SYSDATE()
-WHERE INTERFACE_NAME = 'INT_SSP_DEMAND_DETAIL_BIMA_IMP'
-{% endmacro %}
--- dbt_project/macros/ssp_bima_import/update_a_interface_run_header.sql
+++
@@ -1,5 +0,0 @@
-{% macro update_a_interface_run_header() %}
-UPDATE CBI_METADATA.A_INTERFACE_RUN
-SET LAST_RUN_DATE = SYSDATE()
-WHERE INTERFACE_NAME = 'INT_SSP_DEMAND_HEADER_BIMA_IMP'
-{% endmacro %}
